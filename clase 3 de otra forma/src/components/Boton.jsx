fe9e282fef27cc7fb6489d79ca6d3b5e59db9239
castrobrian123 <castrobrian123@outlook.com>
2025-09-12 16:46:22 -0300

    ejercicios react

--- clase 3 de otra forma/src/components/Boton.jsx
+++
@@ -1,8 +0,0 @@
-export const Boton = ({ texto, color }) => {
-  const estilos = {
-    backgroundColor: color,
-    color: "white",
-  };
-
-  return <button style={estilos}>{texto}</button>;
-};
--- clase 3 de otra forma/src/components/Footer/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export const Footer = () => {
-  return (
-    <footer>
-      <p>Pagina creada por Beluzita</p>
-    </footer>
-  );
-};
