c3e94e95c0761eaecb2374e7b1aeb006f87e2ea9
castrobrian123 <castrobrian123@outlook.com>
2025-09-28 10:00:27 -0300

    ejercicios react

--- clase 5/vite.config.js
+++ clase 5/vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) return 'react'
+            if (id.includes('lodash')) return 'lodash'
+            if (id.includes('chart.js')) return 'charts'
+            return 'vendor'
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000 // opcional, para evitar el warning
+  }
 })
+
